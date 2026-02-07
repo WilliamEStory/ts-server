17da0f7d2da67d1f127ed909333c0f723df62008
William Story <william.story.e@gmail.com>
2026-02-06 18:13:45 -0700

    Add middleware for counting file hits under /app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import express from "express";
 
+import { handlerMetrics } from "./api/metrics.js";
 import { handlerReadiness } from "./api/readiness.js";
+import { handlerReset } from "./api/reset.js";
 import { middlewareLogResponses } from "./middleware/middlewareLogging.js";
+import { middlewareMetricsInc } from "./middleware/middlewareMetricsInc.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc);
 
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerMetrics);
+app.get("/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
