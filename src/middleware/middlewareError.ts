065097e01cdfff92e288c230a4868baf7b21024b
William Story <william.story.e@gmail.com>
2026-02-07 23:31:55 -0700

    Use respondWithError helper function

--- src/middleware/middlewareError.ts
+++ src/middleware/middlewareError.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
+import { respondWithError } from "../api/json.js";
+
 export const middlewareError = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
-    res.status(500).json({ "error": "Something went wrong on our end" });
+    respondWithError(res, 500, "Something went wrong on our end");
 }
