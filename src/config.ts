ed99ad8ae3e5280f861c20e5c6a1e88058e7a1c4
William Story <william.story.e@gmail.com>
2026-02-08 17:06:07 -0700

    Create init drizzle orm setup

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 type APIConfig = {
-  fileserverHits: number;
+  fileServerHits: number;
+  dbURL: string;
+};
+
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
 }
 
 export const config: APIConfig = {
-  fileserverHits: 0,
-}
+  fileServerHits: 0,
+  dbURL: envOrThrow("DB_URL"),
+};
